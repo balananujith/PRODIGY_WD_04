c62818fd77930904348f47ee7edeba5cb9c37237
ANUJITH BALAN <118455793+balananujith@users.noreply.github.com>
2023-10-06 20:54:25 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,13 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Anujith Balan</span>a passionate web and Android developer with a strong desire to create innovative and user-friendly digital experiences. I'm dedicated to staying updated with the latest industry trends and best practices to ensure that my work reflects the current standards of web and mobile development. My goal is to not only meet the technical requirements of a project but also to provide elegant solutions that enhance the user experience. I'm excited to collaborate on new projects, tackle challenges, and contribute my skills to make the digital world a more exciting and functional place. Let's connect and discuss how we can work together to bring your ideas to life!"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
+
+  
+
+  
